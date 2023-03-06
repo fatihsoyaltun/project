c66b675e836a3934142d7b91f602bfb83834345e
fatihsoyaltun <soyaltunfatih@gmail.com>
2023-03-06 15:22:52 +0300

    js add

--- app.js
+++ app.js
@@ -66,8 +66,66 @@
   });
   
 
-  // window.addEventListener('load', function() {
-  //   setTimeout(function() {
-  //     loading.style.display = 'none';
-  //   }, 2000);
-  // });
+  window.addEventListener('load', function() {
+    setTimeout(function() {
+      loading.style.display = 'none';
+    }, 2000);
+  });
+
+
+// Soru dizisindeki indeks için sayaç
+let questionIndex = 0;
+
+// Soruları görüntülemek için fonksiyon
+function showQuestion() {
+  // Soruları json aldım
+  fetch('./questions/questionsHtml.json')
+    .then(response => response.json())
+    .then(data => {
+      // Soru, cevap seçenekleri ve yanıt butonu için elementleri seçtim
+      let question = document.querySelector(".soru");
+      let answerA = document.querySelector(".answerA"); 
+      let answerB = document.querySelector(".answerB"); 
+      let answerC = document.querySelector(".answerC"); 
+      let answerD = document.querySelector(".answerD");
+      let next = document.querySelector(".yanitla_buton");
+
+      // İlk soruyu otomatik getirdim
+      question.innerHTML = data[questionIndex].question;
+      answerA.innerHTML = data[questionIndex].a;
+      answerB.innerHTML = data[questionIndex].b;
+      answerC.innerHTML = data[questionIndex].c;
+      answerD.innerHTML = data[questionIndex].d;
+      questionIndex++;
+
+      // Diğer sorular butona tıklayınca gelecek
+      function showNextQuestion() {
+        if (questionIndex < data.length) { 
+          question.innerHTML = data[questionIndex].question;
+          answerA.innerHTML = data[questionIndex].a;
+          answerB.innerHTML = data[questionIndex].b;
+          answerC.innerHTML = data[questionIndex].c;
+          answerD.innerHTML = data[questionIndex].d;
+          questionIndex++;
+
+          // Tüm inputlardan işaretleri kaldırdım
+          document.querySelectorAll("input[type='radio']").forEach(input => {
+            input.checked = false;
+          });
+        } else {
+          question.innerHTML = "Sorular bitti, sonuçları görüntüleyin.";
+          next.removeEventListener("click", showNextQuestion); // Yanıt butonunu devre dışı bıraktım
+        }
+      }
+
+      // Yanıt butonu
+      next.addEventListener("click", showNextQuestion);
+    });
+}
+
+// Sayfa yüklendiğinde soruları görüntüleniyor
+window.onload = showQuestion;
+
+
+
+
